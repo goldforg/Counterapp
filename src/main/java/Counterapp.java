6741182e28180b4dae542e2156cf49946d972123
Rolands Voitins <rolands.voitins@gmail.com>
2015-03-16 15:01:19 +0200

    First commit

---
+++ src/main/java/Counterapp.java
@@ -0,0 +1,31 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+public class Counterapp {
+
+ 
+
+   public Counterapp(){
+    
+   }
+   public static void main(String []args){
+	System.out.println("Hi");
+
+	Properties prop = new Properties();
+
+	String s = "C:\\Counterapp\\src\\main\\config.properties";
+
+	
+
+
+
+
+
+   }
+
+
+}
